33e643e4a5d7ff9aae37fa1037354fed742c92c1
김형래 <hyungrae0907@minigate.net>
2023-11-27 17:34:49 +0900

    typed js 추가

---
+++ public/JS/typejs.js
@@ -0,0 +1,12 @@
+$(function(){
+    var typed2 = new Typed('.typed', {
+        strings: [
+            "HTML5.", "CSS3.", "Sass.", "Bootstrap.", "JavaScript.", "ES6.", "jQuery.", "Node.js.", "Express.", "Socket.io.", "MongoDB.", "TypeScript.", "Angular."
+        ],
+        typeSpeed: 120,
+        startDelay: 0,
+        backSpeed: 80,
+        backDelay: 800,
+        loop: true
+    });
+})

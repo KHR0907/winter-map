aab6198ba987f5805b1cef8d46b9f5c17b7f2511
hyungrae0907 <hyungrae0907@gmail.com>
2023-12-16 03:04:48 +0900

    ojt 최종 결과물

--- public/JS/bicycleApi.js
+++ public/JS/bicycleApi.js
@@ -1,4 +1,5 @@
 const API_KEY= 'xVUaC9zwhd32aSIqjwN1WkJdvpC0qRFSSBGQ8weeuNVvvchGFAZM6C2z6x6u47zALTloPcxi3NzITaL7ouBQjw%3D%3D'
+
 async function getData(){
     const url = `http://apis.data.go.kr/B552061/frequentzoneFreezing/getRestFrequentzoneFreezing?ServiceKey=${API_KEY}&searchYearCd=2021&siDo=&guGun=&type=json&numOfRows=100&pageNo=1`
     const response = await fetch(url)
@@ -13,32 +14,39 @@ async function getData(){
     ]);
 
     console.log("locations",locations)
+    let search = document.getElementById("search").value.toLowerCase();
 
-    drawMap(locations)
+    drawMap(locations, search)
 }
 
-function drawMap(locations){
+function drawMap(locations, search){
+
     const map = new google.maps.Map(document.getElementById("map"),{
         zoom: 13,
         center: new google.maps.LatLng(locations[0][1],locations[0][2]),
         mapTypeId: google.maps.MapTypeId.ROADMAP,
     });
 
+    console.log(search)
     const infowindow = new google.maps.InfoWindow();
 
     let marker, i;
 
 
     for(i = 0; i< locations.length; i++){
-        marker = new google.maps.Marker ({
-            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-            map: map,
-        });
 
-        google.maps.event.addListener(
-            marker,
-            "click",
-            (function (marker, i) {
+        let locationsname= locations[i][0].toLowerCase();
+
+        if(locationsname.includes(search)) {
+
+            marker = new google.maps.Marker ({
+                position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+                map: map,
+            });
+            google.maps.event.addListener(
+                marker,
+                "click",
+                (function (marker, i) {
                     return function () {
 
                         var totalAccidents = locations[i][3] + locations[i][4];
@@ -47,7 +55,6 @@ function drawMap(locations){
 
                         infowindow.setContent(
                             `<h6><b>${locations[i][0]}</b></h6>
-
                         위험도 : 
                         <script type="application/javascript">
                             var warningCount = ${warningCount};
@@ -60,7 +67,16 @@ function drawMap(locations){
                         infowindow.open(map, marker);
                     }
                 })(marker, i)
-        );
+            );
+        }
+
+
+
+
+
     }
 }
+
+
+
 getData();
